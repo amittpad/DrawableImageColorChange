cfd760613b6174c87b104387bae146ecd39cb42b
Amit Pramanik <amit.mca93@gmail.com>
2019-01-19 18:25:47 +0530

    Initial Commit

---
+++ app/src/main/java/com/amittpad/drawableimagecolorchange/MainActivity.java
@@ -0,0 +1,53 @@
+package com.amittpad.drawableimagecolorchange;
+
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView mImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mImage = (ImageView) findViewById(R.id.image);
+        animateImageView(mImage);
+    }
+
+    public void animateImageView(final ImageView v) {
+        final int orange = getResources().getColor(android.R.color.holo_orange_dark);
+
+        final ValueAnimator colorAnim = ObjectAnimator.ofFloat(0f, 1f);
+        colorAnim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float mul = (Float) animation.getAnimatedValue();
+                int alphaOrange = adjustAlpha(orange, mul);
+                v.setColorFilter(alphaOrange, PorterDuff.Mode.SRC_ATOP);
+                if (mul == 0.0) {
+                    v.setColorFilter(null);
+                }
+            }
+        });
+
+        colorAnim.setDuration(1500);
+        colorAnim.setRepeatMode(ValueAnimator.REVERSE);
+        colorAnim.setRepeatCount(-1);
+        colorAnim.start();
+
+    }
+
+    public int adjustAlpha(int color, float factor) {
+        int alpha = Math.round(Color.alpha(color) * factor);
+        int red = Color.red(color);
+        int green = Color.green(color);
+        int blue = Color.blue(color);
+        return Color.argb(alpha, red, green, blue);
+    }
+}
